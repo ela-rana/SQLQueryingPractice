6d4be46df353b613c4221d902ef2254eb63ed229
Ela Rana <elarana3@gmail.com>
2022-08-29 12:10:00 -0400

    Update MedicalManagementSystem.sql

--- MedicalManagementSystem.sql
+++ MedicalManagementSystem.sql
@@ -2,12 +2,11 @@ USE master
 
 CREATE DATABASE MedicalManagementSystem
 
-GO
+GO ------------------------------------
 
 USE MedicalManagementSystem
 
-
-CREATE TABLE SiteUser (
+CREATE TABLE MMSUser (
 	Username varchar(50) NOT NULL,
 	Passwrd varchar(50) NOT NULL,
 	SSN varchar(50) NOT NULL,
@@ -16,36 +15,53 @@ CREATE TABLE SiteUser (
 	LastName varchar(50) NOT NULL,
 	Phone varchar(50) NOT NULL,
 	Email varchar(50) NOT NULL,
-	MailingAddress varchar(100) NOT NULL,
-	DateOfBirth date NOT NULL,
-	UserType varchar(50) NOT NULL
+	UserType int NOT NULL,
 	PRIMARY KEY (Username)
 	)
 
 CREATE TABLE Patient (
+	PatientID int NOT NULL,
 	SSN varchar(50) NOT NULL,
+	MailingAddress varchar(100) NOT NULL,
+	DateOfBirth date NOT NULL,
 	Allergies varchar(50) NOT NULL,
-	PCMID int NOT NULL
+	PCMID int NOT NULL,
+	PRIMARY KEY(PatientID)
 	)
 
 CREATE TABLE Appointment (
 	ApptID int NOT NULL,
 	ApptDate date NOT NULL,
 	ApptTime time NOT NULL,
-	DurationInMinutes int NOT NULL,
+	Duration int NOT NULL,
 	Description varchar(50) NOT NULL,
 	Location varchar(50) NOT NULL,
-	ProviderID int NOT NULL,
-	PatientSSN varchar(50) NOT NULL,
+	PCMID int NOT NULL,
+	PatientID int NOT NULL,
 	PRIMARY KEY (ApptID)
 	)
 
-CREATE TABLE Provider (
-	ProviderID int NOT NULL,
+CREATE TABLE Employee (
+	EmployeeID int NOT NULL,
 	MedCredentials varchar(50) NOT NULL,
 	Specialty varchar(50),
-	SSN varchar(50) NOT NULL
+	SSN varchar(50) NOT NULL,
+	PRIMARY KEY (EmployeeID)
 	)
 
-ALTER TABLE SiteUser
+GO -------------------------------------------
+
+ALTER TABLE MMSUser
 ADD FOREIGN KEY (SSN) REFERENCES Patient(SSN)
+
+ALTER TABLE MMSUser
+ADD FOREIGN KEY (SSN) REFERENCES Employee(SSN)
+
+ALTER TABLE Patient
+ADD FOREIGN KEY (PCMID) REFERENCES Employee(EmployeeID)
+
+ALTER TABLE Appointment
+ADD FOREIGN KEY (PCMID) REFERENCES Employee(EmployeeID)
+
+ALTER TABLE Appointment
+ADD FOREIGN KEY (PatientID) REFERENCES Patient(PatientID)
